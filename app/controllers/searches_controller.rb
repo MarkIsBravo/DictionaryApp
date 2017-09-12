86b46577ff7636eae14986eead86e9e8ad460115
Mark Ma <mazheng1022@hotmail.com>
2017-09-12 13:20:22 -0400

    search controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,17 @@
 class SearchesController < ApplicationController
+    def index
+
+    end
+    def show
+        get_data(params[:id])
+    end
+
+    def get_data (value)
+        @response = HTTParty.get("http://api.pearson.com/v2/dictionaries/entries?headword=#{value}")
+        @selectedResponse = @response['results'].select do |result|
+            result['headword'] = value
+            result['part_of_speech'] != nil
+        end
+        @results = @selectedResponse.sort_by!{|result| result['part_of_speech']}
+    end
 end
