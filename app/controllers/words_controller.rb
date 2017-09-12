545850b19bf9b50b472a9a0b5ce9473b7151cd05
Mark Ma <mazheng1022@hotmail.com>
2017-09-12 11:07:37 -0400

    search word attempt

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,15 @@
+require "httparty"
 class WordsController < ApplicationController
+    def new
+        @getData = get_data(:funky)
+    end
+
+    def get_data (value)
+        @response = HTTParty.get("http://api.pearson.com/v2/dictionaries/entries?headword=#{value}")
+        @selectedResponse = @response['results'].select do |result|
+            result['headword'] = value
+            result['part_of_speech'] != nil
+        end
+        @results = @selectedResponse.sort_by!{|result| result['part_of_speech']}
+    end
 end
