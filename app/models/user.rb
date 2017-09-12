46deaabd1c50293f88a0e7e4e8620c68e40d2624
Mark Ma <mazheng1022@hotmail.com>
2017-09-11 21:48:17 -0400

    complete username and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ApplicationRecord
     validates :password, length: (6..20), 
     
     attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil unless user
+        user.is_password?(password) ? user : nil
+    end
+
+    def is_password?(password_attempt)
+        BCrypt::Password.new(password_digest).is_password?(password_attempt)
+    end
+
+    def password=(raw_password)
+        @password = raw_password
+        self.password_digest = BCrypt::Password.create(raw_password)
+    end
+    
 end
