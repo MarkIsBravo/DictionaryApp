2138f1be4d010cf915307efce0be37befce818b5
Mark Ma <mazheng1022@hotmail.com>
2017-09-12 00:05:28 -0400

    migrate words

--- db/migrate/20170912033824_create_words.rb
+++ db/migrate/20170912033824_create_words.rb
@@ -1,8 +1,15 @@
 class CreateWords < ActiveRecord::Migration[5.1]
   def change
     create_table :words do |t|
+      t.string :spell
+      t.string :part_of_speech
+      t.text :definition
+      t.text :translation
+      t.belongs_to :user
 
       t.timestamps
     end
+
+    add_index :words, :spell
   end
 end
