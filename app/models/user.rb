fa6808b00c1ad99b05b3b82b082313d79d9d43f5
Mark Ma <mazheng1022@hotmail.com>
2017-09-11 21:22:26 -0400

    add validation to username and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+    has_many :words
+
+    validates_presence_of :username
+    validates :username, length: (6..15), uniqueness: true
+    validates :password, length: (6..20), 
+    
+    attr_reader :password
 end
