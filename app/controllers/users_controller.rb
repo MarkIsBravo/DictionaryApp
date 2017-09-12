bc9160ef7e30966028065ec0d70e81b92f05a74d
Mark Ma <mazheng1022@hotmail.com>
2017-09-11 21:42:05 -0400

    create users controller and index, new, create and show.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+    def index
+        @user = User.all 
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.new(create_params)
+
+        if user.save
+            sign_in(user)
+            flash[:notice] = 'Logged in successfully'
+            redirect_to users_path
+        else
+            errors = user.errors.full_messages
+            flash[:error] = errors.join(', ')
+            @user = User.new(username: create_params[:username])
+            render :new
+        end
+    end
+
+    private
+
+    def create_params
+        params.require(:user).permit(:username, :password)
+    end
 end
