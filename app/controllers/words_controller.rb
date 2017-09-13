d24669d350a2c8f0dee9ff51cfd23cc44bf95175
Mark Ma <mazheng1022@hotmail.com>
2017-09-12 21:40:52 -0400

    wordscontroller setup

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,32 @@
-require "httparty"
 class WordsController < ApplicationController
+    before_action :ensure_logged_in
+    before_action :load_words, only: [:show, :edit, :update, :destroy]
+    def index
+        @words = current_user.words
+    end
     
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end 
+
+    def destroy
+    end
+
+    private
+
+    def load_words
+        @word = current_user.words.find(params[:id])
+    end
+
 end
